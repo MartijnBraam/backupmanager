0bf7317913f75884e4fd24a2e79454a06f177dbf
Martijn Braam <martijn@brixit.nl>
2016-02-15 10:27:18 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
         name='backupmanager',
-        version='0.1.0',
+        version='0.2.0',
         packages=['backupmanager', 'backupmanager.tools'],
         url='https://github.com/MartijnBraam/backupmanager',
         license='MIT',
